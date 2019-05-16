163fb48e8c81f9742bfd9daf2378b04603d3e4a5
Rishabh Jain <it.rishabh.jain@gmail.com>
2019-05-16 12:23:16 +0530

    made base structure
    Made a base structure with scenes and a standard Loader to load new scenes in a circular pattern.
    
    Plan to create UI Slider and Async Load next and update the code to remain dynamic using Build Index

---
+++ Unity Project (Loading Async Scene)/Assets/Scripts/ChangeColorOnCol.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorOnCol : MonoBehaviour
+{
+
+	Material myMaterial;
+	float red, green, blue;
+	public Color myCol;
+	void Start()
+	{
+		//create a new material using the material attached to the gameObject
+		myMaterial = new Material(GetComponent<Renderer>().material);
+
+		//change the material used by the game Object.
+		//set the newly created material as the material used by this GameObject
+		GetComponent<Renderer>().material = myMaterial;
+	}
+
+	void OnCollisionEnter(Collision colEvent)
+	{
+		//create random color
+		red = Random.Range(0.2f, 0.8f);
+		green = Random.Range(0.2f, 0.8f);
+		blue = Random.Range(0.2f, 0.8f);
+		myCol = new Color(red, green, blue);
+
+		//assign random color to material
+		myMaterial.color = myCol;
+	}
+}
---
+++ Unity Project (Loading Async Scene)/Assets/Scripts/StandardLoadScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StandardLoadScene : MonoBehaviour {
+
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			SceneManager.LoadScene(
+				//check current built index : SceneManager.GetActiveScene().buildIndex
+				//add one to this to go to next scene
+				//divide the number by scene count to get back to circular order
+				(SceneManager.GetActiveScene().buildIndex + 1)
+				%
+				SceneManager.sceneCountInBuildSettings
+				);
+		}
+	}
+}
