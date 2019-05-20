4c1dec10c139e916c3b55e09d7dd414aa23dc6ef
Rishabh Jain <it.rishabh.jain@gmail.com>
2019-05-20 17:45:08 +0530

    Some More code to Async Load
    As scene is small, tried adding particles and other data to make it slower so we can see the progress bar. Need to use this in a bigger scene in future.

---
+++ Unity Project (Loading Async Scene)/Assets/Scripts/ManuallyDelayNewSceneLoad.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManuallyDelayNewSceneLoad : MonoBehaviour 
+{
+	public int a = 50;
+	public int b = 200;
+	GameObject cubes, spheres;
+	
+	void Awake()
+	{
+		Method03();
+	}
+	
+	void Start()
+	{
+		 Method01();
+	}
+	
+	
+	void Method01()
+	{
+		if(a >= 0)
+		{
+			cubes = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			a--;
+			Method02();
+		}
+		else if(cubes)
+			Destroy(cubes);
+	}
+	
+	void Method02()
+	{
+		if(a >= 0)
+		{
+			Destroy(cubes);
+			a--;
+			Method01();
+		}
+		else if(cubes)
+			Destroy(cubes);
+	}
+	
+	void Method03()
+	{
+		if(b >= 0)
+		{
+			spheres = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			b--;
+			Method04();
+		}
+		else if(spheres)
+			Destroy(spheres);
+	}
+	
+	void Method04()
+	{
+		if(b >= 0)
+		{
+			Destroy(spheres);
+			b--;
+			Method03();
+		}
+		else if(spheres)
+			Destroy(spheres);
+	}
+}
---
+++ Unity Project (Loading Async Scene)/Assets/Scripts/SceneLoadProgress.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoadProgress : MonoBehaviour
+{
+	public Slider progressBar;
+	public Text ProgressText;
+	
+	void Start()
+	{
+		progressBar.gameObject.SetActive(false);
+	}
+
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			LoadNextScene();
+		}
+	}
+
+	void LoadNextScene()
+	{
+		
+		progressBar.gameObject.SetActive(true);
+		//use the coroutine to load the scene with next builtIndex
+		StartCoroutine(LoadNewScene());
+
+	}
+
+	IEnumerator LoadNewScene()
+	{
+
+		// Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
+		AsyncOperation async = SceneManager.LoadSceneAsync(
+				(SceneManager.GetActiveScene().buildIndex + 1)
+				%
+				SceneManager.sceneCountInBuildSettings
+		
+		);
+
+		// While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
+		while (!async.isDone)
+		{
+			float progress = Mathf.Clamp01(async.progress / 0.9f);
+			progressBar.value = progress;
+			ProgressText.text = progress * 100f + "%";
+			yield return null;
+		}
+	}
+}
